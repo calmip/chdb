5be98d953d97e3537c554bced5db264c96bf5b7c
Emmanuel Courcelle <emmanuel.courcelle@toulouse.inra.fr>
2014-11-17 13:09:34 +0100

    molscript.bash updated (using intel mpi rather than bullx mpi on eos)

--- molscript/molscript.bash
+++ molscript/molscript.bash
@@ -7,11 +7,14 @@
 ### #SBATCH --mail-user=toto@mail.com
 ### #SBATCH --mail-type=ALL
 
+module purge
+module load intel intelmpi
+
 #
 # utilisation de chdb avec molauto/molscript pour generer un fichier postscript 
 # pour chaque entree de la pdb
 #
-# Exemple de script utilisable avec slurm
+# Exemple de script utilisable avec slurm sur eos
 #
 # La commande suivante permet d'afficher le resultat:
 # ghostscript $(find ps -type f)
@@ -19,12 +22,5 @@
 # cf. http://www.avatar.se/molscript/
 #
 
-module purge
-module load bullxmpi
-
-export OMPI_MCA_ess=^pmi
-export OMPI_MCA_pubsub=^pmi
-export OMPI_MCA_mpi_leave_pinned=1
-
-srun --resv-ports chdb --in-dir pdb --in-type ent --out-dir ps --out-files %out-dir%/%path% --sort-by-size --command "./molauto %in-dir%/%path% |./molscript -ps >%out-dir%/%dirname%/%basename%.ps"
+mpirun chdb --in-dir pdb --in-type ent --out-dir ps --out-files %out-dir%/%path% --sort-by-size --command "./molauto %in-dir%/%path% |./molscript -ps >%out-dir%/%dirname%/%basename%.ps"
 
