97ee68a3be41a749bccbd08d2aac418e07b09105
Emmanuel Courcelle <emmanuel.courcelle@inp-toulouse.fr>
2017-06-02 17:49:55 +0200

    Ajout des fichiers de modules dans l'installation

--- install.sh
+++ install.sh
@@ -10,11 +10,23 @@
 DST=$1
 MPI=$2
 VERSION=$(awk '/CHDB_VERSION/ {print $3}' version.hpp )
-BINDIR="$DST/local/chdb/$MPI/$VERSION/bin"
+
 EXE="chdb.exe"
 CHDB=chdb
 WRAPPER='slave-wrapper.sh'
 WRAPDIR="slave-wrapper/$MPI/"
 
+BINDIR="$DST/local/chdb/$MPI/$VERSION/bin"
+
+# TODO - Arranger ca pour la prod !!!
+MODULEDIR=~/privatemodules/chdb/$MPI
+MODULEFILE="$MODULEDIR/$VERSION"
+MODULESRCFILE="modulefiles/chdb/$MPI/VERSION"
+
 cp "$EXE" "$WRAPDIR/$CHDB" "$WRAPDIR/$WRAPPER" "$BINDIR"
+sed -e "s!BINDIR!$BINDIR!" -e 's/VERSION/$VERSION/' <$MODULESRCFILE >$MODULEFILE
+
 ( cd $BINDIR; chmod 755 $EXE $CHDB $WRAPPER )
+chmod 644 $MODULEFILE
+
+
